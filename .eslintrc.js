0e6dec248684fcafda2249d6168c8ce3423e7eed
Andrei Shved <svensk.tut.by@gmail.com>
2022-01-05 20:48:55 +0300

    init: react-hooks

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+    extends: [
+        'airbnb-typescript',
+        'airbnb/hooks',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:jest/recommended',
+        'prettier',
+        'plugin:prettier/recommended',
+    ],
+    plugins: ['react', '@typescript-eslint', 'jest', 'import'],
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true,
+    },
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 13,
+        sourceType: 'module',
+        project: './tsconfig.json',
+    },
+    overrides: [
+        {
+            files: ['**/*.tsx'],
+            rules: {
+                'no-return-await': 'off',
+                'react/prop-types': 'off',
+            },
+        },
+    ],
+    rules: {
+        'linebreak-style': 'off',
+        'import/prefer-default-export': 'off',
+        'import/no-default-export': 'off',
+        'import/no-cycle': 'warn',
+        'import/no-extraneous-dependencies': ['warn', { devDependencies: ['**/*.test.js', '**/*.spec.js'] }],
+        'no-debugger': 'warn',
+        'react/react-in-jsx-scope': 'off',
+        'react/destructuring-assignment': 'off',
+        'react/jsx-props-no-spreading': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto',
+            },
+        ],
+    },
+};
---
+++ README.md
@@ -0,0 +1,18 @@
+# React Hooks
+
+A set of useful React hooks that can be installed via NPM
+
+- [x] useInput
+- [x] useTabs
+- [x] useTitle
+- [x] useClick
+- [x] useHover
+- [x] useConfirm
+- [x] usePreventLeave
+- [x] useBeforeLeave
+- [x] useFadeIn
+- [x] useNetwork
+- [x] useScroll
+- [x] useFullscreen
+- [x] useNotifications
+- [x] useAxios
