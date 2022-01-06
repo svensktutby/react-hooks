d65da1b7187741128357efe386a4afe22a59bfb8
Andrei Shved <svensk.tut.by@gmail.com>
2022-01-06 22:28:29 +0300

    feat: useTitle

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,8 @@
 import React, { FC } from 'react';
-import useTabs from './hooks/useTabs';
-
-const fetchContent = [
-    {
-        tab: 'Part 1',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-    },
-    {
-        tab: 'Part 2',
-        text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco',
-    },
-];
+import useTitle from './hooks/useTitle';
 
 export const App: FC = () => {
-    const { currentItem, changeItem } = useTabs(0, fetchContent);
-
-    return (
-        <div className="app" style={{ width: 600, margin: '0 auto' }}>
-            {fetchContent?.map(({ tab }, idx) => (
-                <button key={idx} onClick={() => changeItem(idx)}>
-                    {tab}
-                </button>
-            ))}
-            <div>{currentItem.text}</div>
-        </div>
-    );
+    const titleUpdater = useTitle('Loading...');
+    setTimeout(titleUpdater, 2000, 'Main page');
+    return <div className="app" style={{ width: 600, margin: '0 auto' }}></div>;
 };
